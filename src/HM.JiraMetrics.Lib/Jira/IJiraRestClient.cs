0714eb88d971e948b6266d77002d1b89b8a9b830
rasmusra <yacrumb@yahoo.se>
2015-02-21 11:58:00 +0100

    Revert "first version after move from hm"
    This reverts commit b021ab2a11ac116e87882644bd486ad40c5d7f8d.

--- src/HM.JiraMetrics.Lib/Jira/IJiraRestClient.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace HM.JiraMetrics.Lib.Jira
-{
-    public interface IJiraRestClient
-    {
-        List<string> GetJsonChunks(string jql);
-    }
-}
--- src/HM.JiraMetrics.Lib/Jira/IRestClient.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace HM.OT.JiraMetrics.Lib.Jira
-{
-    public interface IRestClient
-    {
-        List<string> GetJsonChunks(string jql);
-    }
-}
