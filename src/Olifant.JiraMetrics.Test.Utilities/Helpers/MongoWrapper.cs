25b852701815c64cb99826b1bc508a11589c1a28
rasmusra <yacrumb@yahoo.se>
2015-04-21 21:46:58 +0200

    adding missing file #7

---
+++ src/Olifant.JiraMetrics.Test.Utilities/Helpers/MongoWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Olifant.JiraMetrics.Lib.Jira.Model;
+
+namespace Olifant.JiraMetrics.Test.Utilities.Helpers
+{
+    public class MongoWrapper
+    {
+        private MongoConnectionStringBuilder MongoConnectionStringBuilder { get; set; }
+        private static MongoWrapper _instance;
+
+        private MongoWrapper(string connectionString)
+        {
+            MongoConnectionStringBuilder = new MongoConnectionStringBuilder(connectionString);
+        }
+
+        public static void Init(string connectionString, IList<Issue> createFromFiles)
+        {
+            _instance = new MongoWrapper(connectionString);
+        }
+
+        public static MongoWrapper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new InvalidOperationException("Need to init this singleton first");
+                }
+                return _instance;
+            }
+        }
+
+        public void InitTestPopulation(IList<Issue> issues)
+        {
+            DropDatabase();
+            GetCollection<Issue>().InsertBatch(issues);
+        }
+
+        public MongoCollection<T> GetCollection<T>()
+        {
+            var client = new MongoClient(MongoConnectionStringBuilder.ConnectionString);
+            var server = client.GetServer();
+            var db = server.GetDatabase(MongoConnectionStringBuilder.DatabaseName);
+            return db.GetCollection<T>(typeof(T).Name.ToLower());
+        }
+
+        private void DropDatabase()
+        { 
+            var client = new MongoClient(MongoConnectionStringBuilder.ConnectionString);
+            var server = client.GetServer();
+
+            if (server.DatabaseExists(MongoConnectionStringBuilder.DatabaseName))
+            {
+                server.DropDatabase(MongoConnectionStringBuilder.DatabaseName);
+            }
+        }
+    }
+}
