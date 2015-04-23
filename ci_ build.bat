62ced420d2710366c053c2517ec762bedc6f5497
rasmusra <yacrumb@yahoo.se>
2015-04-23 18:03:50 +0200

    added bat file for CI server

--- ci_ build.bat
+++ ci_ build.bat
@@ -1,10 +1,11 @@
 @echo off
 
-
 if NOT EXIST "tools\FAKE*" (
 	"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
 ) ELSE (
 	ECHO "FAKE is installed!"
 )
+
+tools/FAKE/tools/FAKE.exe build.fsx
 
 
