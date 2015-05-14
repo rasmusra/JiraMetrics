0d058799daf9e06f429a62daf06af2cded5dfe73
Rasmus <yacrumb@yahoo.se>
2015-05-14 08:20:20 +0200

    moved scripts to subfolder

---
+++ scripts/ci_build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+if NOT EXIST "tools\FAKE*" (
+	"..\tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "..\tools" "-ExcludeVersion"
+) ELSE (
+	ECHO "FAKE is installed!"
+)
+
+%~dp0"..\tools\FAKE\tools\FAKE.exe" ci_build.fsx %*
+
+
--- scripts/core_build.fsx
+++ scripts/core_build.fsx
@@ -1,12 +1,13 @@
     // include Fake lib
-#r @"tools\FAKE\tools\FakeLib.dll"
+#r @"..\tools\FAKE\tools\FakeLib.dll"
 open Fake
 
 // Properties
-let srcRoot = "src"
-let outputDllFiles = !! "**/bin/**/*.dll"
-let projectFiles = !! "**/*.csproj"
-let buildArtifacts = String.concat @"\" [__SOURCE_DIRECTORY__; "buildArtifacts"]
+let projRoot = String.concat "/" [__SOURCE_DIRECTORY__ ; ".."]
+let srcRoot = projRoot + "/src"
+let outputDllFiles = !! (srcRoot + "/**/bin/**/*.dll")
+let projectFiles = !! (srcRoot + "/**/*.csproj")
+let buildArtifacts = projRoot + "/buildArtifacts"
 let testResultFile = "testResults.xml"
 let testReport = buildArtifacts + "/testReport.html"
 let featuresDir = buildArtifacts + "/features"
@@ -17,20 +18,20 @@ let showResultInBrowser = getBuildParamOrDefault "showResultInBrowser" "yes"
 let mongoPath = @"C:\Program Files\MongoDB\Server\3.0\bin"
 let mongoPort = getBuildParamOrDefault "port" "27113"
 let mongoDb = "JiraMetricsDb"
-let mongoDbPath = String.concat @"\" [__SOURCE_DIRECTORY__; "data" ; "db"]
-let mongoLogDir = String.concat @"\" [__SOURCE_DIRECTORY__; "data" ; "log" ]
-let nunitPath = @"packages\NUnit.Runners.2.6.4\tools"
+let mongoDbPath = projRoot + "/data/db"
+let mongoLogDir = projRoot + "/data/log"
+let nunitPath = projRoot + "/packages/NUnit.Runners.2.6.4/tools"
 
 // Targets  
 Target "Clean" (fun _ ->
-    trace __SOURCE_DIRECTORY__
     DeleteFiles outputDllFiles
     CleanDir buildArtifacts
 )
 
 Target "Publish specs" (fun _ ->
-    let args = "-f src/Olifant.JiraMetrics.Test.Acceptance/Features" + @" -o " + featuresDir
-    let errorCode = Shell.Exec("packages/Pickles.CommandLine.1.0.0/tools/pickles.exe", args)
+    let picklesArgs = "-f " + srcRoot + "/Olifant.JiraMetrics.Test.Acceptance/Features -o " + featuresDir
+    let pickles = projRoot + "/packages/Pickles.CommandLine.1.0.0/tools/pickles.exe"
+    let errorCode = Shell.Exec(pickles, picklesArgs)
     ()
 )
 
@@ -50,16 +51,16 @@ Target "Build" (fun _ ->
             ]
     }
 
-  build setParams "Olifant.JiraMetrics.sln"
+  build setParams (projRoot + "/Olifant.JiraMetrics.sln")
     |> DoNothing
 )
 
 Target "Test" (fun _ ->
     ActivateFinalTarget "Publish test report"
 
-    !! (srcRoot + @"\**\bin\Debug\*.Test.Unit.dll")
-    ++ (srcRoot + @"\**\bin\Debug\*.Test.Acceptance.dll")
-    -- (srcRoot + @"\**\bin\Debug\*.Fakes.dll")
+    !! (srcRoot + "/**/bin/Debug/*.Test.Unit.dll")
+    ++ (srcRoot + "/**/bin/Debug/*.Test.Acceptance.dll")
+    -- (srcRoot + "/**/bin/Debug/*.Fakes.dll")
     |> NUnit (fun p -> 
     {
         p with 
@@ -73,7 +74,8 @@ Target "Test" (fun _ ->
 
 FinalTarget "Publish test report" (fun _ ->
     let nunitOrangeArgs = testResultFile + " " + testReport
-    Shell.Exec("packages/NUnitOrange.2.1/tools/nunitorange.exe", nunitOrangeArgs)
+    let nunitOrange = projRoot + "/packages/NUnitOrange.2.1/tools/nunitorange.exe"
+    Shell.Exec(nunitOrange, nunitOrangeArgs)
     |> ignore
 
     match showResultInBrowser with
@@ -87,18 +89,16 @@ FinalTarget "Publish test report" (fun _ ->
 Target "StartMongoDb" (fun _ ->
     CreateDir mongoDbPath
     CreateDir mongoLogDir
-    let logfile = String.concat @"\" [ mongoLogDir ; mongoDb + ".log" ]
-    let cmd = mongoPath + @"\mongod"
-    let args = "--dbpath " + mongoDbPath + " --logpath " + logfile + " --port " + mongoPort
-    trace (cmd + " " + args)
-    let errorCode = Shell.Exec(cmd, args)
+    let logfile = String.concat "/" [ mongoLogDir ; mongoDb + ".log" ]
+    let mongodArgs = "--dbpath " + mongoDbPath + " --logpath " + logfile + " --port " + mongoPort
+    let errorCode = Shell.Exec(mongoPath + "/mongod", mongodArgs)
     ()
 )
 
 // populates mongo db with the test data dump file
 Target "SetupMongoDb" (fun _ ->
-    let args = "-d " + mongoDb + " -c issue --file src\Olifant.JiraMetrics.Test.Utilities\Stubs\jirametricsdb_dump.json --upsert -h localhost:" + mongoPort
-    let errorCode = Shell.Exec(mongoPath + @"\mongoimport", args)
+    let args = "-d " + mongoDb + " -c issue --file " + srcRoot + "/Olifant.JiraMetrics.Test.Utilities/Stubs/jirametricsdb_dump.json --upsert -h localhost:" + mongoPort
+    let errorCode = Shell.Exec(mongoPath + "/mongoimport", args)
     ()
 )
 
